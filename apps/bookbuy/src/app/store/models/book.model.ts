f4a227626771c7b874e3105e1195cd3f4bdf967a
NivedhaSVGit <nivema16@gmail.com>
2021-03-26 13:30:55 +0530

    Revert "Revert "Revert "Revert "Revert "Review changes"""""
    This reverts commit 8ea52c2e50647e7ce56472ea7b8fc3bc99e95b6a.

--- apps/bookbuy/src/app/store/models/book.model.ts
+++ apps/bookbuy/src/app/store/models/book.model.ts
@@ -15,11 +15,3 @@ export interface Book {
     saleInfo: any;
     accessInfo: any;
 }
-
-export interface NavItem {
-    displayName: string;
-    disabled?: boolean;
-    iconName: string;
-    route?: string;
-    tag?: boolean;
-}
